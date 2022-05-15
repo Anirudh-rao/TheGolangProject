b283c5f3351631f8660891b134154db44dacbc0f
Anirudh Rao <anirudh131299@gmail.com>
2022-05-15 15:09:44 +0530

    Variables in go

---
+++ 1.Basics/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Variable Declaration
+	var a = "initial"
+	fmt.Println(a)
+
+	//integer declaration
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	//Boolean Declartion
+	var d = true
+	fmt.Println(d)
+
+	//integer declaration
+	var e int
+	fmt.Println(e)
+
+	//String initialization
+	f := "apple"
+	fmt.Println(f)
+
+}
